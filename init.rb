61632b5047774cc044ee783e722052d6020779be
root <root@bns02.(none)>
2011-01-12 16:57:57 +0900

    create plugin skel

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_dnd_upload do
+  name 'Redmine Dnd Upload plugin'
+  author 'hiromitz'
+  description 'plugin for file drag and drop upload.'
+  version '0.0.1'
+end
